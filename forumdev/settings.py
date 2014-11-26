7389c3ccc8f8ed8919e41a79b8ffc15b48720035
Maik Berchten <mberchten@crisler.ch>
2014-11-26 08:14:01 +0100

    changed file permissions to 774 (not sure if correct though?)

--- forumdev/settings.py
+++ forumdev/settings.py
@@ -215,9 +215,9 @@ CMS_PLACEHOLDER_CONF = {}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'newms',                      # Or path to database file if using sqlite3.
+        'NAME': 'forumdev',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'newms',
+        'USER': 'fduser',
         'PASSWORD': 'mb27199',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
