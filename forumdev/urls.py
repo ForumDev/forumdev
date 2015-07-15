359fd63cc2da013af202c5dc310a04cb53fcedba
Maik Berchten <mberchten@crisler.ch>
2015-07-15 22:55:06 +0200

    no language (/en/) in url anymore

--- forumdev/urls.py
+++ forumdev/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import *  # NOQA
-from django.conf.urls.i18n import i18n_patterns
+#from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls import patterns
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.conf import settings
@@ -9,7 +10,7 @@ from myauth.forms import AuthLoginForm
 admin.site.login_form = AuthLoginForm 
 admin.autodiscover()
 
-urlpatterns = i18n_patterns('',
+urlpatterns = patterns('',
     url(r'^admin/publications/publication/import_bibtex/$', 'publications.admin_views.import_bibtex'),
     url(r'^admin/', include(admin.site.urls)),  # NOQA
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
